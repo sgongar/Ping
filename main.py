abe8f4eea3562ba29ae9300f030c7f1b1ba94e9d
sgongar <s.gongoragarcia@gmail.com>
2019-07-11 18:02:04 +0200

    actualizacion

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
-# import RPi.GPIO as GPIO
-# import os
-# import sys
 from bluetooth import *
 from time import sleep
-# from subprocess import call
-# import pyrebase
 
 from firebase import send_time, set_up_firebase
 import I2C_LCD_driver
@@ -64,23 +59,6 @@ class PingDetect:
         :return:
         """
         self.mylcd.lcd_display_string(msg, row, 0)
-
-    def scroll_info(self, msg):
-        """ todo, hay un loop infinito
-
-        :param msg:
-        :return:
-        """
-        str_pad = " " * 16
-        my_long_string = msg
-        my_long_string = str_pad + my_long_string
-
-        while True:
-            for i in range (0, len(my_long_string)):
-                lcd_text = my_long_string[i:(i+16)]
-                self.mylcd.lcd_display_string(lcd_text, 2)
-                sleep(0.2)
-                self.mylcd.lcd_display_string(str_pad, 2)
 
 
 if __name__ == "__main__":
